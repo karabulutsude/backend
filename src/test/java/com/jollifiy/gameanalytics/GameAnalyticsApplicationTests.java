14cafdb9dc3090cfe6ad856a92d219d29bafe144
karabulutsude <karabulutsude.2005@gmail.com>
2026-07-29 14:28:57 +0300

    test: H2 veritabani konfigurasyonu eklendi

--- src/test/java/com/jollifiy/gameanalytics/GameAnalyticsApplicationTests.java
+++ src/test/java/com/jollifiy/gameanalytics/GameAnalyticsApplicationTests.java
@@ -2,12 +2,14 @@ package com.jollifiy.gameanalytics;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Test profilini devreye sokar
 class GameAnalyticsApplicationTests {
 
     @Test
     void contextLoads() {
+        // Bu test sadece Spring Context'in başarılı bir şekilde ayağa kalkıp kalkmadığını kontrol eder.
     }
-
 }
