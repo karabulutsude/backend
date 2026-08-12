08ed64d0c8fb1eac419e670d54caee7a4bed6d9a
karabulutsude <karabulutsude.2005@gmail.com>
2026-08-12 14:55:28 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { overrideVaadinConfig } from './vite.generated';
 
-export default defineConfig({
-    build: {
-        rollupOptions: {
-            external: ['workbox-build'],
+export default defineConfig((env) => {
+    const baseConfig = overrideVaadinConfig(env);
+    return {
+        ...baseConfig,
+        build: {
+            ...baseConfig.build,
+            rollupOptions: {
+                ...baseConfig.build?.rollupOptions,
+                external: ['workbox-build'],
+            },
         },
-    },
+    };
 });
