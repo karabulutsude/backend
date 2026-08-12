47e210d01c734d278b2094aed8984344bfd3edf0
karabulutsude <karabulutsude.2005@gmail.com>
2026-08-12 15:27:54 +0300

    vite.generate

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,11 @@ export default defineConfig((env) => {
     const baseConfig = overrideVaadinConfig(env);
     return {
         ...baseConfig,
-        // Workbox ve PWA eklentisinin build aşamasında hata vermesini engeller
         build: {
             ...baseConfig.build,
             rollupOptions: {
                 ...baseConfig.rollupOptions,
-                external: ['workbox-build'],
+                external: ['workbox-build', 'workbox-core', 'workbox-precaching'],
             },
         },
     };
